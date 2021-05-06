5ec1134c51b7136a91209e228102b20c6e0d7a65
stephaniewilhite <wilhiteinfo@gmail.com>
2021-05-06 15:08:21 -0500

    changes

---
+++ tech_new/Assets/Scripts/musicplaying.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicplaying : MonoBehaviour
+
+    
+{
+    public AudioSource WinterSound;
+    public ParticleSystem Snow;
+    public GameObject GroundSnow;
+    public GameObject Flowers;
+
+    bool isGroundSnowShowing = false;
+    bool isFlowerShowing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WinterSound.Play();
+        Snow.Play();
+        GroundSnow.SetActive(true);
+        isGroundSnowShowing = true;
+        Flowers.SetActive(true);
+        isFlowerShowing = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        WinterSound.Stop();
+        Snow.Stop();
+        GroundSnow.SetActive(false);
+        isGroundSnowShowing = false;
+        Flowers.SetActive(false);
+        isFlowerShowing = false;
+
+    }
+
+
+}
